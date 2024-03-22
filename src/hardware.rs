3ff150aa7c4f4517e2e2d63f046be3d1bcd9cbcb
red2fred2 <red2fred2base@live.com>
2024-03-22 02:30:41 -0500

    Initial commit

---
+++ src/hardware.rs
@@ -0,0 +1,97 @@
+//! Handles most low level hardware abstraction
+
+use cortex_m::delay::Delay;
+use rp2040_hal::{clocks::init_clocks_and_plls, pac, usb::UsbBus, Clock, Sio, Watchdog};
+use usb_device::class_prelude::UsbBusAllocator;
+
+use crate::usb_manager::UsbManager;
+
+static mut SINGLETON: Option<Hardware> = None;
+
+pub struct Hardware {
+	delay: Delay,
+	pins: rp2040_hal::gpio::Pins,
+	usb: Option<UsbManager>,
+	usb_bus: UsbBusAllocator<UsbBus>,
+}
+
+impl Hardware {
+	/// Initialize RP2040 hardware
+	pub fn init(crystal_frequency: u32) {
+		critical_section::with(|_| {
+			let mut pac = pac::Peripherals::take().unwrap();
+			let core = pac::CorePeripherals::take().unwrap();
+			let mut watchdog = Watchdog::new(pac.WATCHDOG);
+			let sio = Sio::new(pac.SIO);
+
+			let clocks = init_clocks_and_plls(
+				crystal_frequency,
+				pac.XOSC,
+				pac.CLOCKS,
+				pac.PLL_SYS,
+				pac.PLL_USB,
+				&mut pac.RESETS,
+				&mut watchdog,
+			)
+			.ok()
+			.unwrap();
+
+			let delay;
+			let usb;
+			let usb_bus;
+
+			unsafe {
+				delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
+
+				usb_bus = UsbBusAllocator::new(UsbBus::new(
+					pac.USBCTRL_REGS,
+					pac.USBCTRL_DPRAM,
+					clocks.usb_clock,
+					true,
+					&mut pac.RESETS,
+				));
+
+				// Enable the USB interrupt
+				pac::NVIC::unmask(rp2040_hal::pac::Interrupt::USBCTRL_IRQ);
+			};
+
+			let pins = rp2040_hal::gpio::Pins::new(
+				pac.IO_BANK0,
+				pac.PADS_BANK0,
+				sio.gpio_bank0,
+				&mut pac.RESETS,
+			);
+
+			unsafe {
+				SINGLETON = Some(Hardware {
+					delay,
+					pins,
+					usb: None,
+					usb_bus,
+				});
+
+				usb = UsbManager::new(&SINGLETON.as_ref().unwrap().usb_bus);
+
+				SINGLETON.as_mut().unwrap().usb = Some(usb);
+			}
+		})
+	}
+
+	/// Get the hardware singleton
+	pub fn get() -> Option<&'static mut Self> {
+		unsafe { SINGLETON.as_mut() }
+	}
+
+	pub fn get_delay(&mut self) -> &mut Delay {
+		&mut self.delay
+	}
+
+	pub fn get_pins(&mut self) -> &mut rp2040_hal::gpio::Pins {
+		&mut self.pins
+	}
+
+	pub fn get_usb(&mut self) -> &mut UsbManager {
+		// It should be impossible for this to be None, panic if it is
+		self.usb.as_mut().unwrap()
+	}
+}
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+#![no_std]
+#![no_main]
+#[link_section = ".boot2"]
+#[no_mangle]
+#[used]
+static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
+
+#[cfg(feature = "rt")]
+extern crate cortex_m_rt;
+
+pub mod hardware;
+pub mod serial_logger;
+pub mod usb_manager;
+
+use hardware::Hardware;
+use log::info;
+use panic_reset as _;
+use rp2040_hal::entry;
+use serial_logger::SerialLogger;
+
+#[entry]
+fn main() -> ! {
+    let crystal_frequency = 12_000_000;
+    Hardware::init(crystal_frequency);
+    let hardware = Hardware::get().unwrap();
+
+    SerialLogger::init(log::LevelFilter::Info);
+
+    let mut number: u32 = 0;
+
+    loop {
+        info!("Running for {number} seconds");
+        number += 1;
+
+        hardware.get_delay().delay_ms(1000);
+    }
+}
