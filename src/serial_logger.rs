7d04c284a0f5a0d98e7034eb20c3cec1253607aa
red2fred2 <red2fred2base@live.com>
2024-03-23 21:13:29 -0500

    Convert indentation to spaces and update dependencies

--- src/serial_logger.rs
+++ src/serial_logger.rs
@@ -18,92 +18,98 @@ static mut LOGGER: Option<SerialLogger> = None;
 pub struct SerialLogger {}
 
 impl SerialLogger {
-	/// Creates a new logger
-	pub fn new() -> SerialLogger {
-		SerialLogger {}
-	}
+    /// Creates a new logger
+    pub fn new() -> SerialLogger {
+        SerialLogger {}
+    }
 
-	/// Sets up the log interface after a logger is created
-	///
-	/// If this fails, it'll just disable logging and hope everything works out.
-	pub fn init(level: LevelFilter) {
-		unsafe {
-			LOGGER = Some(SerialLogger::new());
-			let logger = LOGGER.as_ref().unwrap();
+    /// Sets up the log interface after a logger is created
+    ///
+    /// If this fails, it'll just disable logging and hope everything works out.
+    pub fn init(level: LevelFilter) {
+        unsafe {
+            LOGGER = Some(SerialLogger::new());
+            let logger = LOGGER.as_ref().unwrap();
 
-			critical_section::with(|_| {
-				let result = log::set_logger_racy(logger);
-				log::set_max_level_racy(level);
+            critical_section::with(|_| {
+                let result = log::set_logger_racy(logger);
+                log::set_max_level_racy(level);
 
-				// Disable logging if it fails to set up
-				if result.is_err() {
-					FAILED_INIT = true;
-				}
-			});
-		}
-	}
+                // Disable logging if it fails to set up
+                if result.is_err() {
+                    FAILED_INIT = true;
+                }
+            });
+        }
+    }
 
-	/// Write the trailing part of the message
-	///
-	/// This part will reset color code, then add a carriage return and newline.
-	fn write_affix() {
-		// Skip this if hardware isn't set up yet
-		let Some(hardware) = Hardware::get() else {return};
-		let usb = hardware.get_usb();
+    /// Write the trailing part of the message
+    ///
+    /// This part will reset color code, then add a carriage return and newline.
+    fn write_affix() {
+        // Skip this if hardware isn't set up yet
+        let Some(hardware) = Hardware::get() else {
+            return;
+        };
+        let usb = hardware.get_usb();
 
-		let result = usb.write_str("\x1b[0m\r\n");
+        let result = usb.write_str("\x1b[0m\r\n");
 
-		if result.is_err() {
-			warn!("Failed to write log affix");
-		}
-	}
+        if result.is_err() {
+            warn!("Failed to write log affix");
+        }
+    }
 
-	/// Writes the color escape code for this log level
-	fn write_coloring(level: &Level) -> () {
-		// Skip this if hardware isn't set up yet
-		let Some(hardware) = Hardware::get() else {return};
-		let usb = hardware.get_usb();
+    /// Writes the color escape code for this log level
+    fn write_coloring(level: &Level) -> () {
+        // Skip this if hardware isn't set up yet
+        let Some(hardware) = Hardware::get() else {
+            return;
+        };
+        let usb = hardware.get_usb();
 
-		let color_string = match level {
-			Level::Error => "\x1b[31;1m",
-			Level::Warn => "\x1b[33;1m",
-			Level::Info => "\x1b[37m",
-			Level::Debug => "\x1b[35m",
-			Level::Trace => "\x1b[36m",
-		};
+        let color_string = match level {
+            Level::Error => "\x1b[31;1m",
+            Level::Warn => "\x1b[33;1m",
+            Level::Info => "\x1b[37m",
+            Level::Debug => "\x1b[35m",
+            Level::Trace => "\x1b[36m",
+        };
 
-		let result = usb.write_str(color_string);
+        let result = usb.write_str(color_string);
 
-		if result.is_err() {
-			warn!("Failed to write log color escape");
-		}
-	}
+        if result.is_err() {
+            warn!("Failed to write log color escape");
+        }
+    }
 
-	/// Write the message part of the log
-	fn write_message(message: &Arguments) {
-		// Skip this if hardware isn't set up yet
-		let Some(hardware) = Hardware::get() else {return};
-		let usb = hardware.get_usb();
+    /// Write the message part of the log
+    fn write_message(message: &Arguments) {
+        // Skip this if hardware isn't set up yet
+        let Some(hardware) = Hardware::get() else {
+            return;
+        };
+        let usb = hardware.get_usb();
 
-		let args = message.clone();
-		let result = usb.write_fmt(args);
+        let args = message.clone();
+        let result = usb.write_fmt(args);
 
-		if result.is_err() {
-			warn!("Failed to write log message");
-		}
-	}
+        if result.is_err() {
+            warn!("Failed to write log message");
+        }
+    }
 }
 
 impl Log for SerialLogger {
-	fn enabled(&self, _metadata: &log::Metadata) -> bool {
-		true
-	}
+    fn enabled(&self, _metadata: &log::Metadata) -> bool {
+        true
+    }
 
-	fn log(&self, record: &log::Record) {
-		Self::write_coloring(&record.level());
-		Self::write_message(record.args());
-		Self::write_affix();
-	}
+    fn log(&self, record: &log::Record) {
+        Self::write_coloring(&record.level());
+        Self::write_message(record.args());
+        Self::write_affix();
+    }
 
-	fn flush(&self) {}
+    fn flush(&self) {}
 }
--- src/usb_manager.rs
+++ src/usb_manager.rs
@@ -1,10 +1,9 @@
 //! Handles low level USB stuff
 use rp2040_hal as hal;
 use rp2040_hal::pac::interrupt;
-use usb_device;
 use usb_device::{
-	bus::UsbBusAllocator,
-	device::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
+    bus::UsbBusAllocator,
+    device::{UsbDevice, UsbDeviceBuilder, UsbVidPid},
 };
 use usbd_serial::SerialPort;
 
@@ -12,59 +11,58 @@ use crate::hardware::Hardware;
 
 /// Deals with low level USB stuff
 pub struct UsbManager {
-	device: UsbDevice<'static, hal::usb::UsbBus>,
-	serial: SerialPort<'static, hal::usb::UsbBus>,
+    device: UsbDevice<'static, hal::usb::UsbBus>,
+    serial: SerialPort<'static, hal::usb::UsbBus>,
 }
 
 impl UsbManager {
-	pub fn new(usb_bus: &'static UsbBusAllocator<hal::usb::UsbBus>) -> Self {
-		let serial = usbd_serial::SerialPort::new(usb_bus);
+    pub fn new(usb_bus: &'static UsbBusAllocator<hal::usb::UsbBus>) -> Self {
+        let serial = usbd_serial::SerialPort::new(usb_bus);
 
-		let device = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x0000, 0x000b))
-			.manufacturer("red2fred2")
-			.product("ws2812b controller")
-			.serial_number("01")
-			.device_class(2)
-			.device_protocol(1)
-			.build();
+        let device = UsbDeviceBuilder::new(usb_bus, UsbVidPid(0x0000, 0x000b))
+            .device_class(2)
+            .device_protocol(1)
+            .build();
 
-		UsbManager { device, serial }
-	}
+        UsbManager { device, serial }
+    }
 
-	/// Handles USB reads
-	///
-	/// All this does is dump the data in a buffer and walk away. Once the buffer
-	/// fills, it'll just miss anything new.
-	pub unsafe fn interrupt(&mut self) {
-		if self.device.poll(&mut [&mut self.serial]) {
-			let mut data: [u8; 256] = [0x00; 256];
+    /// Handles USB reads
+    ///
+    /// All this does is dump the data in a buffer and walk away. Once the buffer
+    /// fills, it'll just miss anything new.
+    pub unsafe fn interrupt(&mut self) {
+        if self.device.poll(&mut [&mut self.serial]) {
+            let mut data: [u8; 256] = [0x00; 256];
 
-			let _ = self.serial.read(&mut data);
-		}
-	}
+            let _ = self.serial.read(&mut data);
+        }
+    }
 }
 
 // Fmt implementation for USB writes
 impl core::fmt::Write for UsbManager {
-	fn write_str(&mut self, s: &str) -> core::fmt::Result {
-		let result = self.serial.write(s.as_bytes());
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        let result = self.serial.write(s.as_bytes());
 
-		// Ignore the result, because if it's handled, it'll just create more
-		// data recursively and blow up.
-		match result {
-			_ => (),
-		}
+        // Ignore the result, because if it's handled, it'll just create more
+        // data recursively and blow up.
+        match result {
+            _ => (),
+        }
 
-		Ok(())
-	}
+        Ok(())
+    }
 }
 
 #[allow(non_snake_case)]
 #[interrupt]
 unsafe fn USBCTRL_IRQ() {
-	// If usb isn't enabled by the time an interrupt is attempted, just ignore
-	// it and hope nothing bad happens.
-	let Some(hardware) = Hardware::get() else {return};
+    // If usb isn't enabled by the time an interrupt is attempted, just ignore
+    // it and hope nothing bad happens.
+    let Some(hardware) = Hardware::get() else {
+        return;
+    };
 
-	hardware.get_usb().interrupt()
+    hardware.get_usb().interrupt()
 }
