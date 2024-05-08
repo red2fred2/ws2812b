119d7fc399fced921cf9a2f2b2e467eb34c0bb0f
red2fred2 <red2fred2base@live.com>
2024-05-08 11:11:58 -0500

    Add simple heap allocator

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,10 @@ static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_GENERIC_03H;
 #[cfg(feature = "rt")]
 extern crate cortex_m_rt;
 
+extern crate alloc;
+
 pub mod hardware;
+    use core::mem::MaybeUninit;
 pub mod pio;
 pub mod tx;
 pub mod rx;
@@ -16,14 +19,20 @@ pub mod serial_logger;
 pub mod state_machine;
 pub mod usb_manager;
 
+use embedded_alloc::Heap;
 use hardware::Hardware;
 use log::info;
 use panic_reset as _;
 use rp2040_hal::entry;
 use serial_logger::SerialLogger;
 
+#[global_allocator]
+static HEAP: Heap = Heap::empty();
+
 #[entry]
 fn main() -> ! {
+    init_allocator();
+
     let crystal_frequency = 12_000_000;
     Hardware::init(crystal_frequency);
     let hardware = Hardware::get().unwrap();
@@ -38,4 +47,10 @@ fn main() -> ! {
 
         hardware.get_delay().delay_ms(1000);
     }
+}
+
+fn init_allocator() {
+    const HEAP_SIZE: usize = 128 * 1024;
+    static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
+    unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
 }
